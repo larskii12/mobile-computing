9296d408344e754b2603ab5fe2a0636cb45f6a54
Shouyi Yuan <shouyiy@student.unimelb.edu.au>
2023-10-07 22:55:41 +1100

    Database connection template
    Connect to database successfully.

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.mainactivity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        // 设置按钮监听器
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            DatabaseHelper db = new DatabaseHelper();
+                            if (db.databaseConnectionTest()) {
+                                System.out.println("Database online!");
+                                System.out.println(db.getAllUsers());
+                            } else {
+                                System.out.println("Database offline!");
+                            }
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+                    }
+                }.start();
+            }
+        });
     }
 }
