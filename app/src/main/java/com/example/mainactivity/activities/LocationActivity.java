463ab2bdbb32768420a866c9f2ab407dfca2d24e
Linda <linda.kamau2@gmail.com>
2023-10-17 14:54:11 +1100

    Added location activity page

--- app/src/main/java/com/example/mainactivity/activities/LocationActivity.java
+++ app/src/main/java/com/example/mainactivity/activities/LocationActivity.java
@@ -1,4 +1,16 @@
 package com.example.mainactivity.activities;
 
-public class LocationActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mainactivity.R;
+
+public class LocationActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
 }
