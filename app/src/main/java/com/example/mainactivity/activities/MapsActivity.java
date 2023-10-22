a741b89e4d01f4e560672cded366e4a02bb3b61a
mwwl <moniquewee26@gmail.com>
2023-10-22 16:34:00 +1100

    Merge search page from feat/search-page branch

--- app/src/main/java/com/example/mainactivity/activities/MapsActivity.java
+++ app/src/main/java/com/example/mainactivity/activities/MapsActivity.java
@@ -1,34 +1,107 @@
 package com.example.mainactivity.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SearchView;
+
 import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
-
 import com.example.mainactivity.R;
+import com.example.mainactivity.databinding.ActivityMapsBinding;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.mainactivity.databinding.ActivityMapsBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private SearchView searchBar;
+    private ImageButton filterButton;
+    private BottomNavigationView bottomNav;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
+
         setContentView(binding.getRoot());
+
+        searchBar = findViewById(R.id.searchBar);
+        filterButton = (ImageButton) findViewById(R.id.filterButton);
+        bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.setSelectedItemId(R.id.searchNav);
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        // for the map (mMap), get the nearby locations and display it as a pointer
+        /**
+         *
+         * on below line we are adding marker to that position::
+         * mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+         *
+         *
+         */
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String searchQuery = searchBar.getQuery().toString();
+                Intent intent = new Intent(MapsActivity.this, SearchResults.class);
+                intent.putExtra("searchQuery", searchQuery);
+                startActivity(intent);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // launch the filter page
+                Intent intent = new Intent(MapsActivity.this, FilterAdjustmentActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+//            @SuppressLint("NonConstantResourceId")
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.homeNav:
+//                        // go to home navigation page (Linda's page)
+//                        break;
+//                    case R.id.focusNav:
+//                        // go to focus page
+//                        break;
+//                    case R.id.accountNav:
+//                        // go to account page
+//                        break;
+//                }
+//                return false;
+//            }
+//        });
+
     }
 
     /**
@@ -44,8 +117,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        mMap.setMaxZoomPreference(30);
+        mMap.setMinZoomPreference(15);
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(-37.800097099486244, 144.96440741828778);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
