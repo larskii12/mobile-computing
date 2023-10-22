a5ae591b49621e5893a8430a9dbec41887f008c8
mwwl <moniquewee26@gmail.com>
2023-10-22 15:08:40 +1100

    Created new thread to run out of main UI thread -- in progress

--- app/src/main/java/com/example/mainactivity/activities/SearchResults.java
+++ app/src/main/java/com/example/mainactivity/activities/SearchResults.java
@@ -8,9 +8,11 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 
 import com.example.mainactivity.R;
 import com.example.mainactivity.models.location.Location;
@@ -25,46 +27,80 @@ import com.example.mainactivity.service.location.LocationServiceImpl;
 import com.example.mainactivity.service.resource.ResourceService;
 import com.example.mainactivity.service.resource.ResourceServiceImpl;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.sql.Time;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SearchResults extends AppCompatActivity {
-    LocationService searchAPI = new LocationServiceImpl();
-    ResourceService resourceAPI = new ResourceServiceImpl();
+    private final LocationServiceImpl searchAPI = new LocationServiceImpl();
+    private ResourceServiceImpl resourceAPI = new ResourceServiceImpl();
     LinearLayout resultCardArea;
-    List<Location> results;
+    List<Location> results = new ArrayList<>();
 
     CardView cardView;
-
-    LinearLayout facilities;
-    TextView cardTitle;
-    TextView distanceAway;
-    TextView openingHours;
-    ProgressBar capacity;
-    ImageView closingIcon;
-    ImageView locImage;
-    ImageButton addToFavButton;
-    TextView ratingText;
-
+    HashMap<Integer, List<Object>> locationToUI;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_search_results);
 
         // retrieve what was searched for/filtered for
         Intent intent = getIntent();
-        resultCardArea = findViewById(R.id.resultCardArea);
         String searchString = intent.getStringExtra("searchQuery");
+
+        //results = new ArrayList<>();
+        Toast.makeText(getApplicationContext(), searchString, Toast.LENGTH_SHORT).show();
+        resultCardArea = findViewById(R.id.resultCardArea);
 
         // use API to get the search result
         // study includes both libraries and study spaces
-        try {
-            results = searchAPI.findAllLocations("STUDY", searchString, true);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
 
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    results = searchAPI.findAllLocations("STUDY", searchString, true);
+
+//                    if (results.size() != 0) {
+//                        // display something that says "no available results" --> go back to previous page
+//                    }
+
+                    Location onePlace = results.get(0);
+                    String locName = onePlace.getName();
+                    Toast.makeText(getApplicationContext(), locName, Toast.LENGTH_SHORT).show();
+                    addResultsToPage(results);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }.start();
+
+        // have to move this to another thread....
+//        try {
+//            results = searchAPI.findAllLocations("STUDY", searchString, true);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        for (Location location : results) {
+//            if (location instanceof StudySpace) {
+//                cardView = createStudyLocationCard((StudySpace) location);
+//            } else if (location instanceof Library) {
+//                cardView = createLibraryLocationCard((Library) location);
+//            } else {
+//                cardView = createRestaurantLocationCard((Restaurant) location);
+//            }
+//            resultCardArea.addView(cardView);
+//        }
+    }
+    private void addResultsToPage(List<Location> results) {
         for (Location location : results) {
             if (location instanceof StudySpace) {
                 cardView = createStudyLocationCard((StudySpace) location);
@@ -75,35 +111,40 @@ public class SearchResults extends AppCompatActivity {
             }
             resultCardArea.addView(cardView);
         }
-
     }
 
     private CardView createStudyLocationCard(StudySpace location) {
+        List<Object> interactables = new ArrayList<>();
         int buildingID = location.getBuildingId();
         int locationID = location.getId();
 
-        CardView locationCard = (CardView) LayoutInflater.from(this).inflate(R.layout.large_location_card, null, false);
-        facilities = findViewById(R.id.facilitiesIcons);
-        cardTitle = findViewById(R.id.title);
-        distanceAway = findViewById(R.id.distanceText);
-        openingHours = findViewById(R.id.openingHours);
-        capacity = findViewById(R.id.progressBar);
-        closingIcon = findViewById(R.id.closingIcon);
-        locImage = findViewById(R.id.locImage);
-        addToFavButton = findViewById(R.id.addToFavButton);
-        ratingText = findViewById(R.id.ratingText);
+        CardView locationCard = (CardView) LayoutInflater.from(this).inflate(R.layout.large_location_card, resultCardArea, true);
+        LinearLayout facilities = findViewById(R.id.facilitiesIcons);
+        TextView cardTitle = findViewById(R.id.title);
+        TextView distanceAway = findViewById(R.id.distanceText);
+        TextView openingHours = findViewById(R.id.openingHours);
+        ProgressBar capacity = findViewById(R.id.progressBar);
+        ImageView closingIcon = findViewById(R.id.closingIcon);
+        ImageView locImage = findViewById(R.id.locImage);
+        ImageButton addToFavButton = findViewById(R.id.addToFavButton);
+        TextView ratingText = findViewById(R.id.ratingText);
+
+        interactables.add(capacity);
+        interactables.add(closingIcon);
+        locationToUI.put(locationID, interactables);
 
         // first set the title
         setTitle(location, cardTitle);
 
-        // set the distance away
+        // set the distance away!!!!!
 
         // set the clock colour
-
+        updateClockColour(location);
 
         // set opening hours text
+        setOpeningHoursText(location, openingHours);
 
-        // set rating text
+        // set rating text!!!!!
 
 
         // set icons for facilities available
@@ -116,9 +157,6 @@ public class SearchResults extends AppCompatActivity {
             throw new RuntimeException(e);
         }
 
-        String title = location.getName();
-        cardTitle.setText(title);
-
         // I would need the location of the Location Type - maybe can just use google maps API
 
         return locationCard;
@@ -126,34 +164,41 @@ public class SearchResults extends AppCompatActivity {
 
 
     private CardView createLibraryLocationCard(Library location) {
+        List<Object> interactables = new ArrayList<>();
         int buildingID = location.getBuildingId();
         int locationID = location.getId();
 
-        CardView locationCard = (CardView) LayoutInflater.from(this).inflate(R.layout.large_location_card, null, false);
-        facilities = findViewById(R.id.facilitiesIcons);
-        cardTitle = findViewById(R.id.title);
-        distanceAway = findViewById(R.id.distanceText);
-        openingHours = findViewById(R.id.openingHours);
-        capacity = findViewById(R.id.progressBar);
-        closingIcon = findViewById(R.id.closingIcon);
-        locImage = findViewById(R.id.locImage);
-        addToFavButton = findViewById(R.id.addToFavButton);
-        ratingText = findViewById(R.id.ratingText);
+        CardView locationCard = (CardView) LayoutInflater.from(this).inflate(R.layout.large_location_card, resultCardArea, true);
+        LinearLayout facilities = findViewById(R.id.facilitiesIcons);
+        TextView cardTitle = findViewById(R.id.title);
+        TextView distanceAway = findViewById(R.id.distanceText);
+        TextView openingHours = findViewById(R.id.openingHours);
+        ProgressBar capacity = findViewById(R.id.progressBar);
+        ImageView closingIcon = findViewById(R.id.closingIcon);
+        ImageView locImage = findViewById(R.id.locImage);
+        ImageButton addToFavButton = findViewById(R.id.addToFavButton);
+        TextView ratingText = findViewById(R.id.ratingText);
+
+        interactables.add(capacity);
+        interactables.add(closingIcon);
+        locationToUI.put(locationID, interactables);
 
         // first set the title
         setTitle(location, cardTitle);
 
-        // set the distance away
+        // set the distance away!!!!!!
 
         // set the clock colour
+        updateClockColour(location);
 
         // set opening hours text
+        setOpeningHoursText(location, openingHours);
 
-        // set rating text
+        // set rating text!!!!!!
 
 
         // set icons for facilities available
-
+//
         try {
             List<Resource> libraryResources = resourceAPI.getResourceFromBuilding(buildingID);
         } catch (Exception e) {
@@ -163,50 +208,84 @@ public class SearchResults extends AppCompatActivity {
         return locationCard;
     }
 
+
+    /**
+     * Each location put in hash map from locationID --> [capacity, closingIcon]
+     * @param location
+     * @return
+     */
     private CardView createRestaurantLocationCard(Restaurant location) {
+        List<Object> interactables = new ArrayList<>();
         int buildingID = location.getBuildingId();
         int locationID = location.getId();
 
-        facilities = findViewById(R.id.facilitiesIcons);
-        cardTitle = findViewById(R.id.title);
-        distanceAway = findViewById(R.id.distanceText);
-        openingHours = findViewById(R.id.openingHours);
-        capacity = findViewById(R.id.progressBar);
-        closingIcon = findViewById(R.id.closingIcon);
-        locImage = findViewById(R.id.locImage);
-        addToFavButton = findViewById(R.id.addToFavButton);
-        ratingText = findViewById(R.id.ratingText);
+        LinearLayout facilities = findViewById(R.id.facilitiesIcons);
+        TextView cardTitle = findViewById(R.id.title);
+        TextView distanceAway = findViewById(R.id.distanceText);
+        TextView openingHours = findViewById(R.id.openingHours);
+        ProgressBar capacity = findViewById(R.id.progressBar);
+        ImageView closingIcon = findViewById(R.id.closingIcon);
+        ImageView locImage = findViewById(R.id.locImage);
+        ImageButton addToFavButton = findViewById(R.id.addToFavButton);
+        TextView ratingText = findViewById(R.id.ratingText);
 
+        CardView locationCard = (CardView) LayoutInflater.from(this).
+                inflate(R.layout.large_location_card, resultCardArea, true);
 
-        CardView locationCard = (CardView) LayoutInflater.from(this).inflate(R.layout.large_location_card, null, false);
+        interactables.add(capacity);
+        interactables.add(closingIcon);
+        locationToUI.put(locationID, interactables);
 
         // first set the title
         setTitle(location, cardTitle);
 
-        // set the distance away
+        // set the progress bar capacity -- have the max capacity of each building?
+
+        // set the distance away!!!!
 
         // set the clock colour
+        updateClockColour(location);
 
         // set opening hours text
+        setOpeningHoursText(location, openingHours);
 
-        // set rating text
+        // set rating text!!!!!! 5: VERY BUSY; 1 NOT BUSY AT ALL (accefss from
 
 
         // set icons for facilities available
-
+//
         try {
             List<Resource> restaurantResources = resourceAPI.getResourceFromBuilding(buildingID);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-
         return locationCard;
     }
 
     private void setTitle(Location location, TextView cardTitle) {
         String title = location.getName();
         cardTitle.setText(title);
+    }
+
+    private void setOpeningHoursText(Location location, TextView openingHours) {
+        Time openingTime = location.getOpenTime();
+        Time closingTime = location.getCloseTime();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String openingTimeText = sdf.format(openingTime);
+        String closingTimeText = sdf.format(closingTime);
+
+        String text = "Opening Hours: " + openingTimeText + " - " + closingTimeText;
+        openingHours.setText(text);
+    }
+
+    /**
+     * Wait for Lara's API
+     * @param location
+     * @param ratings
+     */
+    private void setRatingsText(Location location, TextView ratings) {
+
     }
 
     private void addIconsToFacilities(List<Resource> resources, LinearLayout facilities) {
@@ -229,6 +308,45 @@ public class SearchResults extends AppCompatActivity {
             }
         }
     }
+
+    private long calcTimeToClose(Time closingTime) {
+        Date date = new Date();
+        Time currTime = new Time(date.getTime());
+
+        Long timeDiff = TimeUnit.MILLISECONDS.toHours(closingTime.getTime() - currTime.getTime());
+        return timeDiff;
+    }
+
+    private void updateClockColour(Location location) {
+        Time closingTime = location.getCloseTime();
+        Long timeDiff = calcTimeToClose(closingTime);
+
+        int locationID = location.getId();
+        ImageView closingIcon = getClosingIcon(locationID);
+
+        if (timeDiff <= 1) {
+            closingIcon.setColorFilter(ContextCompat.getColor(MainActivity.getAppContext(),
+                    R.color.red), android.graphics.PorterDuff.Mode.SRC_IN);
+            // setColorFilter(getApplicationContext().getResources().getColor.red));
+        } else {
+            closingIcon.setColorFilter(ContextCompat.getColor(MainActivity.getAppContext(),
+                    R.color.green), android.graphics.PorterDuff.Mode.SRC_IN);
+        }
+    }
+
+    private ImageView getClosingIcon(int locationID) {
+        List<Object> interactables = locationToUI.get(locationID);
+        ImageView icon = (ImageView) interactables.get(1);
+        return icon;
+    }
+
+    private ProgressBar getProgressBar(int locationID) {
+        List<Object> interactables = locationToUI.get(locationID);
+        ProgressBar capacity = (ProgressBar) interactables.get(0);
+        return capacity;
+    }
+
+
 
 
 }
