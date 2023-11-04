1b22dbdf21e224631c9b16d3a9dd09f09bbf237d
mwwl <moniquewee26@gmail.com>
2023-11-04 12:04:18 +1100

    Added custom method to convert String to ResourceType

--- uninooks/src/main/java/com/comp90018/uninooks/models/location/resource/ResourceType.java
+++ uninooks/src/main/java/com/comp90018/uninooks/models/location/resource/ResourceType.java
@@ -9,5 +9,26 @@ public enum ResourceType {
     VENDING_MACHINE,
     STOP1,
     EMERGENCY_PHONES,
-    AED;
+    AED,
+    MICROWAVE_OVEN;
+
+
+    public static ResourceType toType(String resourceTypeFromSQL) {
+        String returnString = "";
+        String[] words = resourceTypeFromSQL.split(" ");
+
+        for (int i=0 ; i < words.length ; i++) {
+            returnString += words[i].toUpperCase();
+            if (i != words.length - 1) {
+                returnString = returnString + "_";
+            }
+        }
+
+        for (ResourceType type : ResourceType.values()) {
+            if (type.toString().equals(returnString)) {
+                return type;
+            }
+        }
+        return null;
+    }
 }
