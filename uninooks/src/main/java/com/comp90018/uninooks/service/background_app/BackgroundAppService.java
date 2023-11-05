4a9b118df21a3a24615f08aa8196384851b20f5a
laradelltria <ltria@student.unimelb.edu.au>
2023-11-05 17:41:30 +1100

    Added Background App Sensor

--- uninooks/src/main/java/com/comp90018/uninooks/service/background_app/BackgroundAppService.java
+++ uninooks/src/main/java/com/comp90018/uninooks/service/background_app/BackgroundAppService.java
@@ -1,10 +1,77 @@
 package com.comp90018.uninooks.service.background_app;
 
-import com.comp90018.uninooks.models.background_app.BackgroundApp;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.util.Log;
 
-import java.util.ArrayList;
+import androidx.annotation.Nullable;
 
-public interface BackgroundAppService {
+import com.comp90018.uninooks.R;
 
-    ArrayList<BackgroundApp> getBackgroundApps(long duration);
+public class BackgroundAppService extends Service {
+
+    private Handler handler;
+    private long backgroundStartTime = 0;
+
+    private int checkInterval = 1000;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        handler = new Handler();
+        backgroundStartTime = System.currentTimeMillis();
+        Log.d("BackgroundAppSevice", "I'm here!!");
+        startCheckingInBackgroundTime();
+    }
+
+
+    private void startCheckingInBackgroundTime() {
+        // 1 second
+        handler.postDelayed(timerRunnable, checkInterval);
+    }
+
+    private final Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long backgroundTime = System.currentTimeMillis() - backgroundStartTime;
+            if (backgroundTime > 15000) { // 15 seconds
+                sendNotification();
+            } else {
+                handler.postDelayed(timerRunnable, checkInterval);
+            }
+        }
+    };
+
+    private void sendNotification() {
+        Log.d("BackgroundAppSevice", "I'm notifying!!");
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String channelId = "uninooks";
+        String channelName = "Uninooks Notification";
+
+        Notification.Builder builder;
+
+        NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+        notificationManager.createNotificationChannel(channel);
+        builder = new Notification.Builder(this, channelId);
+
+        builder.setSmallIcon(R.drawable.logo_uninook)
+                .setContentTitle("Still in Study Mode!")
+                .setContentText("Timer is still running. You should get back to studying.");
+
+        notificationManager.notify(1, builder.build());
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
