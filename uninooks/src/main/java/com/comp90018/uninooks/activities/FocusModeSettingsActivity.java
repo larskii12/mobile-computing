4d269127292ba83edfc9b27ba259ebee346115cb
mwwl <moniquewee26@gmail.com>
2023-11-06 14:08:35 +1100

    Return, reset, and save buttons working

--- uninooks/src/main/java/com/comp90018/uninooks/activities/FocusModeSettingsActivity.java
+++ uninooks/src/main/java/com/comp90018/uninooks/activities/FocusModeSettingsActivity.java
@@ -1,11 +1,17 @@
 package com.comp90018.uninooks.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.transition.Fade;
 import androidx.transition.Transition;
@@ -15,6 +21,15 @@ import com.comp90018.uninooks.R;
 
 
 public class FocusModeSettingsActivity extends AppCompatActivity {
+    ImageButton returnButton;
+    Button resetButton;
+    Button saveButton;
+    Switch toggleSwitch;
+    EditText pomodoroTime;
+    EditText shortBreakTime;
+    EditText longBreakTime;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,15 @@ public class FocusModeSettingsActivity extends AppCompatActivity {
 
         LinearLayout pomodoroTimerLayout = findViewById(R.id.LinearLayoutFocusSettingPomodoro);
         final RelativeLayout pomodoroInputsLayout = findViewById(R.id.RelativeLayoutFocusSettingPomodoro);
+
+        returnButton = findViewById(R.id.returnButton);
+        resetButton = findViewById(R.id.resetSettingsButton);
+        saveButton = findViewById(R.id.saveSettingsButton);
+
+        toggleSwitch = findViewById(R.id.toggleSwitch);
+        pomodoroTime = findViewById(R.id.EditTextFocusSettingMins); // these are all in minutes, have to convert to milliiseconds
+        shortBreakTime = findViewById(R.id.EditTextFocusShortBreakSetting);
+        longBreakTime = findViewById(R.id.EditTextFocusLongBreakSetting);
 
         // Set the click listener for the Pomodoro Timer LinearLayout
         pomodoroTimerLayout.setOnClickListener(new View.OnClickListener() {
@@ -45,9 +69,9 @@ public class FocusModeSettingsActivity extends AppCompatActivity {
             }
         });
 
-        LinearLayout shorBreakLayout = findViewById(R.id.LinearLayoutFocusSettingShortBreak);
+        LinearLayout shortBreakLayout = findViewById(R.id.LinearLayoutFocusSettingShortBreak);
         final RelativeLayout shortBreakInputsLayout = findViewById(R.id.RelativeLayoutFocusSettingShortBreak);
-        shorBreakLayout.setOnClickListener(new View.OnClickListener() {
+        shortBreakLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create a transition instance, a fade transition in this case
@@ -89,16 +113,56 @@ public class FocusModeSettingsActivity extends AppCompatActivity {
             }
         });
 
-        Button closeButton = findViewById(R.id.ButtonFocusSettingClose);
-        closeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        returnButton.setOnClickListener(returnListener);
+        resetButton.setOnClickListener(resetListener);
+        saveButton.setOnClickListener(saveListener);
+    }
 
-                finish();
-            }
-        });
+    /**
+     * Goes back to the previous page, no settings are applied/saved
+     */
+    private View.OnClickListener returnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
 
+    /**
+     * Resets all settings to default settings, stays on current page
+     */
+    private View.OnClickListener resetListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            pomodoroTime.setText("");
+            shortBreakTime.setText("");
+            longBreakTime.setText("");
+            toggleSwitch.setChecked(false);
+        }
+    };
 
+    /**
+     * Goes back to previous page with settings applied
+     */
+    private View.OnClickListener saveListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String pomodoro = pomodoroTime.getText().toString();
+            String shortBreak = shortBreakTime.getText().toString();
+            String longBreak = longBreakTime.getText().toString();
+            Boolean onAutoPomodoro = toggleSwitch.isChecked();
+            confirmSettings(pomodoro, shortBreak, longBreak, onAutoPomodoro);
+            finish();
+        }
+    };
 
+    private void confirmSettings(String pomodoro, String shortBreak, String longBreak, Boolean onAutoPomodoro) {
+        SharedPreferences sharedPreferences = getSharedPreferences("MySettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(getString(R.string.pomodoro_setting), Integer.parseInt(pomodoro));
+        editor.putInt(getString(R.string.short_break_setting), Integer.parseInt(shortBreak));
+        editor.putInt(getString(R.string.long_break_setting), Integer.parseInt(longBreak));
+        editor.putBoolean(getString(R.string.auto_pomodoro), onAutoPomodoro);
+        editor.apply();
     }
 }
